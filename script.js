d05e50d995c39af975b9bddde81ff12b37bd69d2
Morgan Zehnder <morganzehnder@gmail.com>
2018-03-14 17:43:46 -0700

    fine touches

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('#persona').each(function() {
+        animationHover(this, 'pulse');
+    });
+});
+
+function animationHover(element, animation){
+    element = $(element);
+    element.hover(
+        function() {
+            element.addClass('animated ' + animation);
+        },
+        function(){
+            //wait for animation to finish before removing classes
+            window.setTimeout( function(){
+                element.removeClass('animated ' + animation);
+            }, 2000);
+        });
+}
